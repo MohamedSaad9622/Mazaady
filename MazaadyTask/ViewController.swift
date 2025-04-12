031fc2e5eed9c4aa2ae57dd6826dafd675f6f3c1
mohamed.khalf <mohamed.saad.khalfallah@gmail.com>
2025-04-12 06:49:12 +0200

    Initial Commit

---
+++ MazaadyTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MazaadyTask
+//
+//  Created by Mohamed Saad on 12/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
